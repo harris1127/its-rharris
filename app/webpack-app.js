8bcdba6304510e9a9cde2ccfc08e2fd93b76805d
Ryan Harris <ryan.e.harris@lowes.com>
2017-05-24 14:20:39 -0400

    Configure webpack; add js files to bundle.js

---
+++ app/webpack-app.js
@@ -0,0 +1,14 @@
+import './lib/jquery.min.js';
+import './lib/tether.min.js';
+import './lib/bootstrap.min.js';
+import './lib/clean-blog.min.js';
+import './lib/angular/angular.js';
+import './lib/angular-ui-router.min.js';
+import './js/app.js';
+import './js/config/rharris-core.module.js';
+import './js/config/rharris-router.js';
+import './js/services.js';
+import './js/controllers.js';
+import './js/filters.js';
+import './js/directives.js';
+import './js/util/rharris-graph.js';
