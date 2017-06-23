75b0d58af8c78100d590e237ac103ccb9ea88bfb
Ryan Harris <ryan.e.harris@lowes.com>
2017-06-23 14:30:27 -0400

    Update styling

--- app/dist/bundle.js
+++ app/dist/bundle.js
@@ -303,8 +303,26 @@ angular.module('rharris.controllers', []).
     function HomeController($scope){
 
         let self = this;
+        let current = 1; //keeps track of the current div
+        let height = $('.roles').height(); //the height of the roles div
+        let numberDivs = $('.roles').children().length; //the number of children of the roles div
+        let first = $('.roles div:nth-child(1)'); //the first div nested in roles div
+
+        setInterval(function() {
+            let number = current * -height;
+            first.css('margin-top', number + 'px');
+            if (current === numberDivs) {
+                first.css('margin-top', '0px');
+                current = 1;
+            } else current++;
+        }, 2000);
 
     }
+
+    //Document Ready
+    $(function(){
+        setInterval();
+    });
 
 
 })();
